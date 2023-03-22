c4451d0e60c67ba38994612b0dfa855dffd99be7
Matheus Reis <matheus.reis081@gmail.com>
2023-03-22 19:55:20 -0300

    feat: listening connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
+const HOST:&str = "127.0.0.1:7878";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(HOST).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
