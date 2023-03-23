d5d189d499ba24fa0eb18fddcd41583787fcbaf4
Matheus Reis <matheus.reis081@gmail.com>
2023-03-23 10:23:57 -0300

    chore: simuling a slow request

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 const HOST: &str = "127.0.0.1:7878";
@@ -27,10 +29,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "templates/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "templates/404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "templates/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "templates/hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "templates/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
